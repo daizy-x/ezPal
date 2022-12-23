45a38340b8c1b848e03b7948cb055c8ef2e19bcc
daizy-x <82283983+daizy-x@users.noreply.github.com>
2022-12-23 12:42:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ window.onload = init;
     let tab_btns = document.getElementsByClassName("tablinks")
     let tabs = document.getElementsByClassName("tabcontent")
     // consider making this a loop and dictionary
-    let home_tab = document.getElementById('Home btn')
+    tab_names = ['Home', 'Reply', 'Collect', 'Stats', 'Settings']
+    for (i=0; i < tab_names.length; i++) {
+        tab = document.getElementById(tab_names[i])
+        tab_btn = document.getElementById(tab_names[i]+" btn")
+        //window.alert(i, tab_names[i])
+        tab_btn.addEventListener("click", (event) => {
+            // close other tabs
+            for (t = 0; t < tabs.length; t++) {
+                tabs[t].style.display = "none";
+            }
+            // mark tabs inactive
+            for (t = 0; t < tab_btns.length; t++) {
+                tab_btns[t].className = tab_btns[t].className.replace("*", '');
+            }
+            // open tab & mark active
+            document.getElementById(event.target.id.replace(' btn', '')).style.display = "block";
+            event.target.className += "*"
+        })
+    }
+
+    /*let home_tab = document.getElementById('Home btn')
     let reply_tab = document.getElementById('Reply btn')
     let collect_tab = document.getElementById('Collect btn')
     let stats_tab = document.getElementById('Stats btn')
@@ -27,7 +47,7 @@ window.onload = init;
         // open this tab and make desc active
         document.getElementById(home_tab.id.replace(' btn', '')).style.display = "block";
         home_tab.className += "*";
-        })
+        })*/
     }
 
 })(window, document, undefined);
